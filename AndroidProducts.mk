8f9f44335825342e3d47639e3d3fb4a4232cc8f9
drkphnx <dark.phnx12@gmail.com>
2022-02-02 12:54:15 +0530

    Alioth:Welcome Awaken to Alioth
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_alioth.mk
+    $(LOCAL_DIR)/awaken_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_alioth-user \
-    nad_alioth-userdebug \
-    nad_alioth-eng
+    awaken_alioth-user \
+    awaken_alioth-userdebug \
+    awaken_alioth-eng
