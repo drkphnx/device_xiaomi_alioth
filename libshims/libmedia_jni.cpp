152bf8ce4b60210b80859cf48e92c3ffbfd1bae7
darkphnx <dark.phnx12@gmail.com>
2022-11-10 14:33:43 +0000

    alioth: add missing symbol for libcameraservice.so
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- libshims/libmedia_jni.cpp
+++ libshims/libmedia_jni.cpp
@@ -3,3 +3,11 @@
 extern "C" { 
      void _ZN7android15checkPermissionERKNS_8String16Eij() {}
 }
+
+extern "C" {
+     void _ZN7android20SensorPrivacyManager20supportsSensorToggleEi() {}
+}
+
+extern "C" {
+     void _ZN7android20SensorPrivacyManager32isIndividualSensorPrivacyEnabledEii() {}
+}
