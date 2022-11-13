f29ebf611abbb7dd6c5908434a867dec634e40d1
∂α૨ҡ_ρɦσεɳเא <dark.phnx12@gmail.com>
2022-11-13 17:47:11 +0530

    Alioth: Bring SparkOS to Alioth
    Signed-off-by: ∂α૨ҡ_ρɦσεɳเא <dark.phnx12@gmail.com>

--- spark_alioth.mk
+++ spark_alioth.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The sparkOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,18 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := spark_alioth
 TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
+
+SPARK_BUILD_TYPE := official
+
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
 
 TARGET_WITH_MATLOGX := false
 
